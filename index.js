7683facd35f5791e7d1bb85c86c1fe907e69e298
Phinjock Sherpa <phinjock@gmail.com>
2022-08-13 17:52:35 -0700

    Readme Generator

---
+++ index.js
@@ -0,0 +1,77 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const inquirer = require("inquirer");
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    name: "title",
+    type: "input",
+    message: "What is the name of this project?",
+  },
+
+  {
+    name: "project",
+    type: "input",
+    message: "Why did you build this project?",
+  },
+  {
+    name: "Installation",
+    type: "input",
+    message: "What was your Installation?",
+    default: "npm i",
+  },
+
+  {
+    name: "Usage",
+    type: "input",
+    message: "Usage:",
+  },
+  {
+    name: "License",
+    type: "checkbox",
+    message: "Which License is it using?",
+    choices: ["bsd", "mit", "apache"], //apache/bsd/MIT
+  },
+  {
+    name: "Contributing",
+    type: "input",
+    message: "Who was the contributer?",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is your email address?",
+  },
+  {
+    name: "Github",
+    type: "input",
+    message: "Please provide githubURL?",
+  },
+  {
+    name: "Test",
+    type: "Input",
+    message: "Please provide how you will run your test?",
+    default: "npm test",
+  },
+];
+
+// TODO: Create a function to write README file // purpose of this function
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log("Success! A readme file has now been created.");
+  });
+}
+
+// // TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+    writeToFile("readme.md", generateMarkdown(data));
+  });
+}
+
+// // Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,80 @@
+// TODO: Create a function that returns a license badge with based on which license is passed in
+// If there is no license, return an empty string
+function generateMarkdown(data) {
+  function renderLicenseBadge(license) {
+    if (license !== "None") {
+      return `![License](https://img.shields.io/badge/license-${license}-blue.svg)
+      `;
+    } else {
+      return "";
+    }
+  }
+
+  function renderLicenseLink(license) {
+    if (license !== "None") {
+      return `https://opensource.org/licenses/${license}`;
+    } else {
+      return "";
+    }
+  }
+
+  return `# ${data.title} 
+  [${renderLicenseBadge(data.License)}](${renderLicenseLink(data.License)})
+  
+  This readme has the following below:
+  1. [Project Descrition](#Project-Description)
+  2. [Usage](#Usage)
+  3. [Contribution](#Contribution)
+  4. [Tests](#Tests)
+  5. [Deployment](#Deployment)
+  6. [Steps Made](#Steps-Made)
+  7. [Questions](#Questions)
+
+### Project Description:
+
+${data.project}
+
+### Usage:
+
+${data.Usage}
+
+### Contribution:
+
+${data.Contributing}
+
+### Tests:
+
+${data.Test}
+
+### Deployment:
+
+[https://github.com/${data.Github}/readmeGenerator]
+
+If any further questions were required relating this readme Generator. You can contact me via email [${
+    data.email
+  }].
+
+### Steps made:
+
+Step 1: Also imported generateMarkdown on index.js locally.
+
+Step 2: To access the following we used the require function.
+
+Step 3: Generated the questionaires to fill in for the readme.
+
+Step 4: Once the information was received then a code using fs module was used to created and write in the readme.md
+
+### Questions
+1. What is the name of this project?
+2. Why did you build this project?
+3. What was your Installation?
+4. Usage:
+5. Which License did you use?
+6. Who was the contributer?
+7. What is your email address?
+7. Please provide githubURL?
+9. Please provide how you will run your test?
+`;
+}
+
+module.exports = generateMarkdown;
